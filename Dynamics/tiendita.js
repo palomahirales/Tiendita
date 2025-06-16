7eaf3c96e5b17bc658e449d2352fb1fb98fc16bd
Santiago-Alv <alvarado.santiago.school@comunidad.unam.mx>
2025-06-16 12:50:29 -0600

    version prefinal

--- Dynamics/tiendita.js
+++ Dynamics/tiendita.js
@@ -1,9 +1,9 @@
 class Articulos{
-    constructor(name,url, precio, id){
+    constructor(name,url, precio, dinero){
         this.name = name;
         this.url = url;
         this.precio = precio;
-        this.id = id;
+        this.dinero = dinero;
     }
     setId(id){
         this.id = id;
@@ -53,11 +53,67 @@ for(let i=0; i<articulos.length; i++){
 
 articulo.innerHTML = html;
 
-for(i=0;i<articulos.length;i++){
-    addBtn[i] = document.getElementById(i);
-    addBtn[i].addEventListener("click", ()=>{
-        articulos[i].getPrecio()
-    });
-}
+let t = 0
+let total = document.getElementById("total");
+let lista = document.getElementById("listacarrito");
+
+let addBtn1 = document.getElementById("1")
+let addBtn2 = document.getElementById("2")
+let addBtn3 = document.getElementById("3")
+let addBtn4 = document.getElementById("4")
+let addBtn5 = document.getElementById("5")
+let addBtn6 = document.getElementById("6")
+let addBtn7 = document.getElementById("7")
+let addBtn8 = document.getElementById("8")
+let addBtn9 = document.getElementById("9")
+
+let c =1;
+addBtn1.addEventListener("click",()=>{
+    t += 250
+    
+    lista.textContent = "X"+c +"Cien años de soledad"
+    total.textContent = String(t)
+    c++;
+});
+
+addBtn2.addEventListener("click",()=>{
+    t += 865
+    total.textContent = String(t)
+});
+
+addBtn3.addEventListener("click",()=>{
+    t += 299
+    total.textContent = String(t)
+});
+
+addBtn4.addEventListener("click",()=>{
+    t += 340
+    total.textContent = String(t)
+});
+
+addBtn5.addEventListener("click",()=>{
+    t += 400
+    total.textContent = String(t)
+});
+
+addBtn6.addEventListener("click",()=>{
+    t += 250
+    total.textContent = String(t)
+});
+
+addBtn7.addEventListener("click",()=>{
+    t += 310
+    total.textContent = String(t)
+});
+
+addBtn8.addEventListener("click",()=>{
+    t += 180
+    total.textContent = String(t)
+});
+
+addBtn9.addEventListener("click",()=>{
+    t += 25
+    total.textContent = String(t)
+});
 
 
